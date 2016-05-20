6cc3261e4e529f9c23c4cbab5b5d00ef14b15f10
Vadim Kuznetzov <ra3xdh@gmail.com>
2016-05-20 15:24:58 +0300

    Added .PARAM substitution for SPICE

--- src/subs_spice_netlist.cpp
+++ src/subs_spice_netlist.cpp
@@ -15,26 +15,45 @@ DEFUN_DLD (subs_spice_netlist, args, nargout,
 
 	std::string s_netlist = args(0).string_value();
 	std::string s_param = args(1).string_value();
+	double p_value = args(2).double_value();
 
 	const char *netlist=s_netlist.c_str();
+	char *tmpfile = (char *) malloc((strlen(netlist)+4)*sizeof(char));
+	strcpy(tmpfile,netlist);
+	strcat(tmpfile,".tmp");
+
 	const char *param=s_param.c_str();
-	FILE *fp = fopen(netlist,"r+");
-	if (!fp) {
+	FILE *src = fopen(netlist,"r");
+	FILE *dst = fopen(tmpfile,"w");
+
+	if ((!src)) {
 	    retval(0) = octave_value(false);
 	    return retval;
 	}
-	while (!feof(fp)) {
+	while (!feof(src)) {
 	    size_t n = NETLIST_MAXLEN;
 	    char *lin = (char *) malloc(NETLIST_MAXLEN*sizeof(char));
-	    getline(&lin,&n,fp);
+	    getline(&lin,&n,src);
 	    char *start = lin;
 	    while (isspace(*start)) start++;
 	    if (*start=='*') continue; // Comment
-	    if (*start=='.') printf("%s",start); // Directive
+	    if (!strncasecmp(start,".PARAM",6)) { // Parameter
+		start += 6; // Start of param;
+		while (isspace(*start)) start++;
+		if (!strncasecmp(start,param,strlen(param))) {
+		    start += strlen(param);
+		    while ((isspace(*start))||(*start=='=')) start++;
+		    sprintf(start,"%g\n",p_value);
+		    printf("%s",lin); // Directive
+		}
+	    }
+	    fputs(lin,dst);
 	    free(lin);
 	}
-	fclose(fp);
+	fclose(src);
+	fclose(dst);
 
+	rename(tmpfile,netlist);
 
 	if (!error_state) {
 	    retval(0) = octave_value(true);
